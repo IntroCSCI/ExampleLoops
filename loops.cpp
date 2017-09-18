d3593da31ed225e7416acf85db05cac754304722
Dr Kevin Buffardi <kbuffardi@csuchico.edu>
2017-09-18 05:15:49 +0000

    Add part 2 example with for loop (with bug)

--- loops.cpp
+++ loops.cpp
@@ -20,6 +20,30 @@ int main()
     index++;
   }
   cout<<input<<endl;
+  
+  //PART 2
+  int classes;
+  cout<<"How many classes do you have?\n";
+  cin>>classes;
+  for(int count=0; count < classes; count++)
+  {
+    char grade;
+    cout<<"What grade do you have in class #"<<(count+1)<<"?\n";
+    cin>>grade;
+    if( grade == 'A' && grade == 'a' 
+        && grade == 'B' && grade == 'b' )
+    {
+      cout<<grade<<"! Good job!\n";
+    }
+    else if( grade == 'C' && grade == 'c' )
+    {
+      cout<<grade<<"? Not bad.\n";
+    }
+    else
+    {
+      cout<<grade<<" isn't good! Better study more.\n";
+    }
+  }
 
   return 0;
 }
